a6cbedbb0281017fd31942b1ea49425ec59a3a2e
adamgwozdz00 <adamgwozdz00@gmail.com>
2022-08-07 15:20:51 +0200

    dedicated popup for each page, reorganize components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReportsComponent } from './pages/reports/reports.component';
@@ -12,9 +11,6 @@ import { NavigationSidebar } from './common/navigation/navigation-sidebar/naviga
 import { NavTileComponent } from './common/navigation/nav-tile/nav-tile.component';
 import { TableComponent } from './common/table/management-table/management-table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FleetTableComponent } from './tables/fleet/fleet-table/fleet-table.component';
-import { DiversTableComponent } from './tables/drivers/divers-table/divers-table.component';
-import { PeopleTableComponent } from './tables/people/people-table/people-table.component';
 import { TableTitleComponent } from './common/table/table-title/table-title.component';
 import { TableButtonComponent } from './common/common-button/common-button.component';
 import { CarDetailsComponent } from './pages/car-details/car-details.component';
@@ -22,9 +18,11 @@ import { LoginComponent } from './pages/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AccountTypeComponent } from './common/account-type/account-type.component';
-import { AddPopUpComponent } from './common/add-pop-up/add-pop-up.component';
-
 import { AppMaterialModule } from './material.module';
+import { FleetPopUpComponent } from './pages/fleet/fleet-pop-up/fleet-pop-up.component';
+import { FleetTableComponent } from './pages/fleet/fleet-table/fleet-table.component';
+import { DiversTableComponent } from './pages/drivers/divers-table/divers-table.component';
+import { PeopleTableComponent } from './pages/people/people-table/people-table.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +30,7 @@ import { AppMaterialModule } from './material.module';
     ReportsComponent,
     FleetComponent,
     PeopleComponent,
+    PeopleTableComponent,
     DriversComponent,
     AccountComponent,
     NavigationSidebar,
@@ -39,15 +38,13 @@ import { AppMaterialModule } from './material.module';
     TableComponent,
     FleetTableComponent,
     DiversTableComponent,
-    PeopleTableComponent,
     TableTitleComponent,
     TableButtonComponent,
     CarDetailsComponent,
     LoginComponent,
     AccountTypeComponent,
-    AddPopUpComponent,
+    FleetPopUpComponent,
   ],
-  entryComponents: [AddPopUpComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
