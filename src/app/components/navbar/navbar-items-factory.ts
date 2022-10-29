345d856dd080ee6589d96f71939319c360223e3e
adamgwozdz00 <adamgwozdz00@gmail.com>
2022-10-29 13:14:03 +0200

    different tabs for different users.

---
+++ src/app/components/navbar/navbar-items-factory.ts
@@ -0,0 +1,26 @@
+import {AccountType} from "../../common/account-type/account-type";
+import {NavbarItem} from "./navbar-item";
+import {FleetRoutes} from "../../common/routes/FleetRoutes";
+
+export class NavbarItemsFactory {
+
+  create(accountType: AccountType): NavbarItem[] {
+    switch (accountType) {
+      case AccountType.ADMIN:
+        return [
+          new NavbarItem("Vehicles", FleetRoutes.VEHICLES),
+          new NavbarItem("Drivers", FleetRoutes.DRIVERS),
+          new NavbarItem("Users", FleetRoutes.USERS),
+          new NavbarItem("Account", FleetRoutes.ACCOUNT),
+          new NavbarItem("Login", FleetRoutes.LOGIN, false, false)
+        ];
+      case AccountType.USER:
+        return [
+          new NavbarItem("Vehicles", FleetRoutes.VEHICLES),
+          new NavbarItem("Drivers", FleetRoutes.DRIVERS),
+          new NavbarItem("Account", FleetRoutes.ACCOUNT),
+          new NavbarItem("Login", FleetRoutes.LOGIN, false, false)
+        ];
+    }
+  }
+}
