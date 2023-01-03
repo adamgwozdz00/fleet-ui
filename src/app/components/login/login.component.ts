0d2311948ab9880d807afb932ad0e7e36d9b174b
Werka Piekacz <weronika.piekacz@interia.pl>
2023-01-03 21:54:40 +0100

    FileInput

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,34 +1,43 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from 'src/app/auth/auth.service';
-import {FormBuilder} from '@angular/forms';
-import {AuthCredentials, Password, Username} from 'src/app/auth/auth-credentials';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/auth/auth.service";
+import { FormBuilder } from "@angular/forms";
+import {
+  AuthCredentials,
+  Password,
+  Username,
+} from "src/app/auth/auth-credentials";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
   signInForm = this.formBuilder.group({
-    username: [''],
-    password: [''],
+    username: [""],
+    password: [""],
   });
 
   constructor(
     private readonly authService: AuthService,
     private formBuilder: FormBuilder
-  ) {
-  }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSignIn() {
-    this.authService.login(this.prepareCredentials())
+    this.authService.login(this.prepareCredentials());
   }
 
   prepareCredentials(): AuthCredentials {
     const rawLoginForm = this.signInForm.getRawValue();
-    return new AuthCredentials(new Username(rawLoginForm["username"]), new Password(rawLoginForm["password"]));
+    return new AuthCredentials(
+      new Username(rawLoginForm["username"]),
+      new Password(rawLoginForm["password"])
+    );
+  }
+
+  readFile(fileContent: string) {
+    console.log(fileContent);
   }
 }
